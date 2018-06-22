ca589073983acebe56b6c75fd1db8036e10686ac
侯恩星 <houenxing@houenxingdeMacBook-Pro.local>
2018-06-22 23:29:15 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-export default {
+  export default {
   created () {
     // 调用API从本地缓存中获取数据
     const logs = wx.getStorageSync('logs') || []
@@ -7,50 +7,58 @@ export default {
     wx.setStorageSync('logs', logs)
     console.log('app created and cache logs by setStorageSync')
 
+    // 登录获取res.code
     wx.login({
       success: function(res) {
-        console.log(res);
+        // console.log(res);
         if (res.code) {
-          //发起网络请求
+          var config = require("../config");
+          // console.log(config.remoteServer);
+
+          // 获取code后向目标服务器发起网络请求，获取openId, sessionKey
           wx.request({
-            url: 'http://127.0.0.1:3006/login',
+            // url: 'http://127.0.0.1:3006/login',
+            url: config.remoteServer + '/login',
             data: {
-              x: 'ss',
+              code: res.code,
             },
             method: 'POST',
+            // 成功获取openId和sessionKey
             success: function(res) {
+              // {
+              //   data: "{"session_key":"CXDjSfDC1UbXcrJZiaQJQQ==","openid":"o_zwE5uazPiMNgixodmYDhOgabjg"}"
+              //   err: null
+              // }
               console.log(res.data)
             },
+            // 获取openId和sessionKey失败
             fail: function() {
               console.log('failed!');
             }
-            // data: {
-            //   code: res.code
-            // }
           })
         } else {
-          console.log('登录失败！' + res.errMsg)
+          console.log('获取用户登录状态失败！' + res.errMsg)
         }
       }
     });
 
-    wx.getSetting({
-      success(res) {
-        if (!res.authSetting['scope.userInfo', 'scope.userLocation']) {
-          wx.authorize({
-            scope: 'scope.record',
-            success() {
-              // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
-              // wx.startRecord()
-              console.log("success!");
-            },
-            fail: function() {
-              console.log("failed!");
-            }
-          })
-        }
-      }
-    })
+    // wx.getSetting({
+    //   success(res) {
+    //     if (!res.authSetting['scope.userInfo', 'scope.userLocation']) {
+    //       wx.authorize({
+    //         scope: 'scope.record',
+    //         success() {
+    //           // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+    //           // wx.startRecord()
+    //           console.log("success!");
+    //         },
+    //         fail: function() {
+    //           console.log("failed!");
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
 
     // wx.setStorage({
     //   key: "name",
